268c40f154ab1c2845b31fc31e728bc2165ddc86
Mathieu Sénidre <matse@skwatt.com>
2023-05-17 08:55:56 +0200

    +className property

--- src/valedit/ValEditObject.hx
+++ src/valedit/ValEditObject.hx
@@ -6,13 +6,15 @@ package valedit;
  */
 class ValEditObject 
 {
+	public var className:String;
 	public var name:String;
 	public var object:Dynamic;
 	
-	public function new(name:String = null, object:Dynamic = null) 
+	public function new(?name:String, ?object:Dynamic, ?className:String) 
 	{
 		this.name = name;
 		this.object = object;
+		this.className = className;
 	}
 	
 }
