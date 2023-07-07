499364dbe2e35350a45ea2626198d04fd1fdc82d
Mathieu Sénidre <matse@skwatt.com>
2023-07-07 17:38:15 +0200

    ValEditTemplate : pooling

--- src/valedit/ValEditTemplate.hx
+++ src/valedit/ValEditTemplate.hx
@@ -7,12 +7,21 @@ import openfl.events.EventDispatcher;
  */
 class ValEditTemplate extends EventDispatcher
 {
+	static private var _POOL:Array<ValEditTemplate> = new Array<ValEditTemplate>();
+	
+	static public function fromPool(clss:ValEditClass, ?id:String, ?object:Dynamic, ?collection:ExposedCollection,
+									?constructorCollection:ExposedCollection):ValEditTemplate
+	{
+		if (_POOL.length != 0) return _POOL.pop().setTo(clss, id, object, collection, constructorCollection);
+		return new ValEditTemplate(clss, id, object, collection, constructorCollection);
+	}
+	
 	public var className:String;
 	public var clss:ValEditClass;
 	public var collection:ExposedCollection;
 	public var constructorCollection:ExposedCollection;
 	public var id(get, set):String;
-	public var numInstances(default, null):Int;
+	public var numInstances(default, null):Int = 0;
 	public var object:Dynamic;
 	
 	private var _id:String;
@@ -27,12 +36,35 @@ class ValEditTemplate extends EventDispatcher
 	public function new(clss:ValEditClass, ?id:String, ?object:Dynamic, ?collection:ExposedCollection, ?constructorCollection:ExposedCollection) 
 	{
 		super();
+		setTo(clss, id, object, collection, constructorCollection);
+	}
+	
+	public function clear():Void
+	{
+		this.clss = null;
+		this.collection = null;
+		this.constructorCollection = null;
+		this.numInstances = 0;
+		this.object = null;
+	}
+	
+	public function pool():Void
+	{
+		clear();
+		_POOL[_POOL.length] = this;
+	}
+	
+	private function setTo(clss:ValEditClass, id:String, object:Dynamic, collection:ExposedCollection,
+						   constructorCollection:ExposedCollection):ValEditTemplate
+	{
 		this.clss = clss;
 		this.className = clss.className;
 		this.id = id;
 		this.object = object;
 		this.collection = collection;
 		this.constructorCollection = constructorCollection;
+		
+		return this;
 	}
 	
 	public function addInstance(instance:ValEditObject):Void
