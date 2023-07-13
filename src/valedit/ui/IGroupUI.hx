f42e43b56db7e7770e07f537644286dd36ea706b
Mathieu Sénidre <matse@skwatt.com>
2023-07-13 11:50:25 +0200

    IGroupUI : addExposedControls & removeExposedControls

--- src/valedit/ui/IGroupUI.hx
+++ src/valedit/ui/IGroupUI.hx
@@ -6,8 +6,10 @@ package valedit.ui;
 interface IGroupUI 
 {
 	function addExposedControl(control:IValueUI):Void;
+	function addExposedControls(controls:Array<IValueUI>):Void;
 	function addExposedControlAfter(control:IValueUI, afterControl:IValueUI):Void;
 	function addExposedControlBefore(control:IValueUI, beforeControl:IValueUI):Void;
 	function removeExposedControl(control:IValueUI):Void;
+	function removeAllExposedControls():Void;
 	function pool():Void;
 }
