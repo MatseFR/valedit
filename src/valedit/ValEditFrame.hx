b194b175eeb0dcfd5acca81b3e5a01a9000fcdcd
Mathieu Sénidre <matse@skwatt.com>
2023-07-25 14:16:15 +0200

    ValEditFrame : added properties & methods

--- src/valedit/ValEditFrame.hx
+++ src/valedit/ValEditFrame.hx
@@ -6,12 +6,45 @@ package valedit;
  */
 class ValEditFrame 
 {
-	public var isEmpty(get, never):Bool;
-	private function get_isEmpty():Bool { return false; }
+	static private var _POOL:Array<ValEditFrame> = new Array<ValEditFrame>();
 	
+	static public function fromPool():ValEditFrame
+	{
+		if (_POOL.length != 0) return _POOL.pop();
+		return new ValEditFrame();
+	}
+	
+	public var hasTween(get, never):Bool;
+	public var indexStart:Int;
+	public var indexEnd:Int;
+	public var isEmpty(get, never):Bool;
 	public var isKeyFrame(default, null):Bool = false;
 	
+	private function get_hasTween():Bool { return false; }
+	private function get_isEmpty():Bool { return true; }
+	
 	public function new() 
+	{
+		
+	}
+	
+	public function clear():Void
+	{
+		
+	}
+	
+	public function pool():Void
+	{
+		clear();
+		_POOL[_POOL.length] = this;
+	}
+	
+	public function enter():Void
+	{
+		
+	}
+	
+	public function exit():Void
 	{
 		
 	}
