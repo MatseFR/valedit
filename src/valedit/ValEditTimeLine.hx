1a2eeca782fcf5afe72e71748961b985083b1747
Mathieu Sénidre <matse@skwatt.com>
2023-08-11 16:34:48 +0200

    ValEditTimeLine : modified/completed according to needs, still WIP

--- src/valedit/ValEditTimeLine.hx
+++ src/valedit/ValEditTimeLine.hx
@@ -1,5 +1,6 @@
 package valedit;
 import juggler.animation.IAnimatable;
+import valedit.utils.ReverseIterator;
 
 /**
  * ...
@@ -22,6 +23,7 @@ class ValEditTimeLine implements IAnimatable
 	public var frameIndex(get, set):Int;
 	public var frameRate(get, set):Float;
 	public var frames(get, never):Array<ValEditKeyFrame>;
+	public var frameTime(get, never):Float;
 	public var loop(get, set):Bool;
 	public var parent(get, set):ValEditTimeLine;
 	
@@ -37,39 +39,33 @@ class ValEditTimeLine implements IAnimatable
 	{
 		if (this._frameIndex == value) return value;
 		
-		//var frame:ValEditKeyFrame = this._frames[value];
-		//if (frame != this._frameCurrent)
-		//{
-			//if (this._frameCurrent != null)// && this._frameCurrent.isKeyFrame)
-			//{
-				//this._frameCurrent.exit();
-			//}
-			//this._frameCurrent = frame;
-			//if (this._frameCurrent != null)// && this._frameCurrent.isKeyFrame)
-			//{
-				//this._frameCurrent.enter();
-			//}
-		//}
+		this._frameIndex = value;
+		
 		setFrameCurrent(this._frames[value]);
 		
 		for (child in this._children)
 		{
-			child.frameIndex = value;
+			child.frameIndex = this._frameIndex;
 		}
 		
-		return this._frameIndex = value;
+		return this._frameIndex;
 	}
 	
 	private var _frameRate:Float;
 	private function get_frameRate():Float { return this._frameRate; }
 	private function set_frameRate(value:Float):Float
 	{
+		if (this._frameRate == value) return value;
+		this._frameTime = 1.0 / value;
 		for (child in this._children)
 		{
 			child.frameRate = value;
 		}
 		return this._frameRate = value;
 	}
+	
+	private var _frameTime:Float;
+	private function get_frameTime():Float { return this._frameTime; }
 	
 	private var _frames:Array<ValEditKeyFrame> = new Array<ValEditKeyFrame>();
 	private function get_frames():Array<ValEditKeyFrame> { return this._frames; }
@@ -92,9 +88,11 @@ class ValEditTimeLine implements IAnimatable
 		return this._parent = value;
 	}
 	
+	private var _numFrames:Int = 0;
+	
 	public function new() 
 	{
-		
+		this.frameRate = 60;
 	}
 	
 	public function clear():Void
@@ -120,6 +118,11 @@ class ValEditTimeLine implements IAnimatable
 		_POOL[_POOL.length] = this;
 	}
 	
+	public function copyFrom(timeLine:ValEditTimeLine):Void
+	{
+		
+	}
+	
 	public function advanceTime(time:Float):Void
 	{
 		
@@ -137,6 +140,7 @@ class ValEditTimeLine implements IAnimatable
 	
 	public function registerKeyFrame(keyFrame:ValEditKeyFrame):Void
 	{
+		keyFrame.timeLine = this;
 		keyFrame.activateFunction = this.activateFunction;
 		keyFrame.deactivateFunction = this.deactivateFunction;
 	}
@@ -155,6 +159,7 @@ class ValEditTimeLine implements IAnimatable
 				this._frameCurrent.enter();
 			}
 		}
+		if (this._frameCurrent != null) this._frameCurrent.indexCurrent = this._frameIndex;
 	}
 	
 	public function addChild(timeLine:ValEditTimeLine):ValEditTimeLine
@@ -184,6 +189,40 @@ class ValEditTimeLine implements IAnimatable
 		return this._children.splice(index, 1)[0];
 	}
 	
+	public function getNextKeyFrame(frame:ValEditKeyFrame):ValEditKeyFrame
+	{
+		for (i in frame.indexEnd + 1...this._numFrames)
+		{
+			if (this._frames[i] != null && this._frames[i] != frame) return cast this._frames[i];
+		}
+		return null;
+	}
 	
+	public function getNextKeyFrameFromIndex(index:Int):ValEditKeyFrame
+	{
+		for (i in index...this._numFrames)
+		{
+			if (this._frames[i] != null) return cast this._frames[i];
+		}
+		return null;
+	}
+	
+	public function getPreviousKeyFrame(frame:ValEditKeyFrame):ValEditKeyFrame
+	{
+		for (i in new ReverseIterator(frame.indexStart -1, 0))
+		{
+			if (this._frames[i] != null && this._frames[i] != frame) return cast this._frames[i];
+		}
+		return null;
+	}
+	
+	public function getPreviousKeyFrameFromIndex(index:Int):ValEditKeyFrame
+	{
+		for (i in new ReverseIterator(index, 0))
+		{
+			if (this._frames[i] != null) return cast this._frames[i];
+		}
+		return null;
+	}
 	
 }
