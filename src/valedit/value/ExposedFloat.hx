f183a3eb716f52419fc7af506918e9e479eb2c31
Mathieu Sénidre <matse@skwatt.com>
2023-08-11 16:36:30 +0200

    ExposedFloat extends ExposedValueTweenable

--- src/valedit/value/ExposedFloat.hx
+++ src/valedit/value/ExposedFloat.hx
@@ -1,5 +1,6 @@
 package valedit.value;
 
+import valedit.value.base.ExposedValueTweenable;
 import valeditor.ui.feathers.variant.TextInputVariant;
 import valedit.value.base.ExposedValue;
 
@@ -7,7 +8,7 @@ import valedit.value.base.ExposedValue;
  * ...
  * @author Matse
  */
-class ExposedFloat extends ExposedValue 
+class ExposedFloat extends ExposedValueTweenable 
 {
 	static private var _POOL:Array<ExposedFloat> = new Array<ExposedFloat>();
 	
