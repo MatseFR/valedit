a52a543d4c5c40f8f2692f86c42985b124e3c1bc
Mathieu Sénidre <matse@skwatt.com>
2023-08-22 17:17:51 +0200

    PropertyMap : clear function + pooling

--- src/valedit/utils/PropertyMap.hx
+++ src/valedit/utils/PropertyMap.hx
@@ -6,12 +6,32 @@ package valedit.utils;
  */
 class PropertyMap 
 {
+	static private var _POOL:Array<PropertyMap> = new Array<PropertyMap>();
+	
+	static public function fromPool():PropertyMap
+	{
+		if (_POOL.length != 0) return _POOL.pop();
+		return new PropertyMap();
+	}
+	
 	private var _objectToRegular:Map<String, String> = new Map<String, String>();
 	private var _regularToObject:Map<String, String> = new Map<String, String>();
 	
 	public function new() 
 	{
 		
+	}
+	
+	public function clear():Void
+	{
+		this._objectToRegular.clear();
+		this._regularToObject.clear();
+	}
+	
+	public function pool():Void
+	{
+		clear();
+		_POOL[_POOL.length] = this;
 	}
 	
 	public function add(regularPropertyName:String, objectPropertyName:String):Void
