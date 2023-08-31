045ba034c60fcb4e526af4ce51632f14bf8d6012
Mathieu Sénidre <matse@skwatt.com>
2023-08-31 11:22:44 +0200

    ContainerData : expose missing ValEditorContainer properties

--- src/valedit/data/valeditor/ContainerData.hx
+++ src/valedit/data/valeditor/ContainerData.hx
@@ -3,6 +3,7 @@ import juggler.animation.Transitions;
 import valedit.ExposedCollection;
 import valedit.value.ExposedBool;
 import valedit.value.ExposedFloatDrag;
+import valedit.value.ExposedIntDrag;
 import valedit.value.ExposedSelect;
 
 /**
@@ -16,8 +17,15 @@ class ContainerData
 	{
 		var bool:ExposedBool;
 		var floatDrag:ExposedFloatDrag;
+		var intDrag:ExposedIntDrag;
 		
 		if (collection == null) collection = new ExposedCollection();
+		
+		if (!collection.hasValue("autoPlay"))
+		{
+			bool = new ExposedBool("autoPlay");
+			collection.addValue(bool, groupName);
+		}
 		
 		if (!collection.hasValue("cameraX"))
 		{
@@ -41,6 +49,42 @@ class ContainerData
 		{
 			floatDrag = new ExposedFloatDrag("y");
 			collection.addValue(floatDrag, groupName);
+		}
+		
+		if (!collection.hasValue("frameRate"))
+		{
+			floatDrag = new ExposedFloatDrag("frameRate");
+			collection.addValue(floatDrag, groupName);
+		}
+		
+		if (!collection.hasValue("numFrames"))
+		{
+			intDrag = new ExposedIntDrag("numFrames", null, 1, null, 1, 1, true, false);
+			collection.addValue(intDrag, groupName);
+		}
+		
+		if (!collection.hasValue("frameIndex"))
+		{
+			intDrag = new ExposedIntDrag("frameIndex");
+			collection.addValue(intDrag, groupName);
+		}
+		
+		if (!collection.hasValue("loop"))
+		{
+			bool = new ExposedBool("loop");
+			collection.addValue(bool, groupName);
+		}
+		
+		if (!collection.hasValue("reverse"))
+		{
+			bool = new ExposedBool("reverse");
+			collection.addValue(bool, groupName);
+		}
+		
+		if (!collection.hasValue("numLoops"))
+		{
+			intDrag = new ExposedIntDrag("numLoops", null, 0);
+			collection.addValue(intDrag, groupName);
 		}
 		
 		if (!collection.hasValue("visible"))
