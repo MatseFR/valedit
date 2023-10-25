3605676021e4a55f550d3f30f3b3d5716b5952d9
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 15:29:40 +0200

    BitmapAsset +toZIPEntry function

--- src/valedit/asset/BitmapAsset.hx
+++ src/valedit/asset/BitmapAsset.hx
@@ -1,5 +1,9 @@
 package valedit.asset;
+import haxe.crypto.Crc32;
+import haxe.io.Bytes;
+import haxe.zip.Entry;
 import openfl.display.BitmapData;
+import openfl.utils.ByteArray;
 
 /**
  * ...
@@ -8,11 +12,26 @@ import openfl.display.BitmapData;
 class BitmapAsset extends Asset 
 {
 	public var content:BitmapData;
+	public var data:ByteArray;
 	public var preview:BitmapData;
 	
 	public function new() 
 	{
 		super();
+	}
+	
+	public function toZIPEntry():Entry
+	{
+		var bytes:Bytes = this.data;
+		return {
+			fileName:this.path,
+			fileSize:bytes.length,
+			fileTime:Date.now(),
+			compressed:false,
+			dataSize:bytes.length,
+			data:bytes,
+			crc32:Crc32.make(bytes)
+		};
 	}
 	
 }
