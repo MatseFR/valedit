446d21e64483825bcb1c3f450fc240dbbdee5835
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 15:28:26 +0200

    BinaryAsset +toZipEntry function

--- src/valedit/asset/BinaryAsset.hx
+++ src/valedit/asset/BinaryAsset.hx
@@ -1,4 +1,7 @@
 package valedit.asset;
+import haxe.crypto.Crc32;
+import haxe.io.Bytes;
+import haxe.zip.Entry;
 import openfl.utils.ByteArray;
 
 /**
@@ -12,6 +15,20 @@ class BinaryAsset extends Asset
 	public function new() 
 	{
 		super();
+	}
+	
+	public function toZIPEntry():Entry 
+	{
+		var bytes:Bytes = this.content;
+		return {
+			fileName:this.path,
+			fileSize:bytes.length,
+			fileTime:Date.now(),
+			compressed:false,
+			dataSize:bytes.length,
+			data:bytes,
+			crc32:Crc32.make(bytes)
+		};
 	}
 	
 }
