f99f5bf4355964351bbd0519145285b7406ddee5
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 15:31:13 +0200

    StarlingTextureAsset +toJSONSave +toZIPEntry

--- src/valedit/asset/starling/StarlingTextureAsset.hx
+++ src/valedit/asset/starling/StarlingTextureAsset.hx
@@ -1,6 +1,9 @@
 package valedit.asset.starling;
+import haxe.zip.Entry;
 import openfl.display.BitmapData;
+import openfl.errors.Error;
 import starling.textures.Texture;
+import valeditor.utils.starling.TextureCreationParameters;
 
 /**
  * ...
@@ -11,10 +14,27 @@ class StarlingTextureAsset extends Asset
 	public var bitmapAsset:BitmapAsset;
 	public var content:Texture;
 	public var preview:BitmapData;
+	public var textureParams:TextureCreationParameters;
 	
 	public function new() 
 	{
 		super();
+	}
+	
+	override public function toJSONSave(json:Dynamic = null):Dynamic 
+	{
+		if (json == null) json = {};
+		
+		json.name = this.name;
+		json.bitmapPath = this.bitmapAsset.path;
+		json.textureParams = this.textureParams.toJSON();
+		
+		return json;
+	}
+	
+	public function toZIPEntry():Entry
+	{
+		throw new Error("no ZIP entry for starling_texture assets");
 	}
 	
 }
