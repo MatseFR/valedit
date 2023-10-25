92bc3a117b15d62d21d5b2b10f450173207ed1fe
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 15:30:42 +0200

    StarlingAtlasAsset +toJSONSave +toZIPEntry

--- src/valedit/asset/starling/StarlingAtlasAsset.hx
+++ src/valedit/asset/starling/StarlingAtlasAsset.hx
@@ -1,10 +1,13 @@
 package valedit.asset.starling;
 
+import haxe.zip.Entry;
 import openfl.display.BitmapData;
+import openfl.errors.Error;
 import starling.textures.TextureAtlas;
 import valedit.asset.Asset;
 import valedit.asset.BitmapAsset;
 import valedit.asset.TextAsset;
+import valeditor.utils.starling.TextureCreationParameters;
 
 /**
  * ...
@@ -16,10 +19,28 @@ class StarlingAtlasAsset extends Asset
 	public var textAsset:TextAsset;
 	public var content:TextureAtlas;
 	public var preview:BitmapData;
+	public var textureParams:TextureCreationParameters;
 
 	public function new() 
 	{
 		super();
+	}
+	
+	override public function toJSONSave(json:Dynamic = null):Dynamic 
+	{
+		if (json == null) json = {};
+		
+		json.name = this.name;
+		json.bitmapPath = this.bitmapAsset.path;
+		json.textPath = this.textAsset.path;
+		json.textureParams = this.textureParams.toJSON();
+		
+		return json;
+	}
+	
+	public function toZIPEntry():Entry
+	{
+		throw new Error("no ZIP entry for starling_atlas assets");
 	}
 	
 }
