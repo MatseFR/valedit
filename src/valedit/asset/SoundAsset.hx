af2864777f2569d9c86967e554cc377ca6b1b5c2
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 15:30:04 +0200

    SoundAsset +toZIPEntry function

--- src/valedit/asset/SoundAsset.hx
+++ src/valedit/asset/SoundAsset.hx
@@ -1,5 +1,9 @@
 package valedit.asset;
+import haxe.crypto.Crc32;
+import haxe.io.Bytes;
+import haxe.zip.Entry;
 import openfl.media.Sound;
+import openfl.utils.ByteArray;
 
 /**
  * ...
@@ -8,10 +12,25 @@ import openfl.media.Sound;
 class SoundAsset extends Asset 
 {
 	public var content:Sound;
+	public var data:ByteArray;
 	
 	public function new() 
 	{
 		super();
+	}
+	
+	public function toZIPEntry():Entry
+	{
+		var bytes:Bytes = this.data;
+		return {
+			fileName:this.path,
+			fileSize:bytes.length,
+			fileTime:Date.now(),
+			compressed:false,
+			dataSize:bytes.length,
+			data:bytes,
+			crc32:Crc32.make(bytes)
+		};
 	}
 	
 }
