09a0b57574dbf030998180c2c11fb353b4b1aa08
Mathieu Sénidre <matse@skwatt.com>
2023-10-31 09:54:35 +0100

    updated SettingsData with FileSettings

--- src/valedit/data/valeditor/SettingsData.hx
+++ src/valedit/data/valeditor/SettingsData.hx
@@ -1,8 +1,13 @@
 package valedit.data.valeditor;
+import juggler.animation.Transitions;
 import valedit.ExposedCollection;
 import valedit.value.ExposedBool;
+import valedit.value.ExposedFloatDrag;
+import valedit.value.ExposedIntDrag;
+import valedit.value.ExposedSelect;
+import valedit.value.ExposedString;
 #if desktop
-import valedit.value.ExposedFilePath;
+import valedit.value.ExposedPath;
 #end
 
 /**
@@ -16,18 +21,31 @@ class SettingsData
 	{
 		var bool:ExposedBool;
 		#if desktop
-		var filePath:ExposedFilePath;
+		var path:ExposedPath;
 		#end
+		var str:ExposedString;
 		
 		if (collection == null) collection = new ExposedCollection();
 		
 		#if desktop
-		if (!collection.hasValue("fullPath"))
+		if (!collection.hasValue("filePath"))
 		{
-			filePath = new ExposedFilePath("fullPath", "export path", false);
-			collection.addValue(filePath, groupName);
+			path = new ExposedPath("filePath", "export path", "export path");
+			collection.addValue(path, groupName);
 		}
 		#end
+		
+		if (!collection.hasValue("fileName"))
+		{
+			str = new ExposedString("fileName", "file name", 0);// , "a-z A-Z 0-9 éèêàâôçùü.-_()");
+			collection.addValue(str, groupName);
+		}
+		
+		if (!collection.hasValue("compress"))
+		{
+			bool = new ExposedBool("compress");
+			collection.addValue(bool, groupName);
+		}
 		
 		if (!collection.hasValue("exportAssets"))
 		{
@@ -48,6 +66,66 @@ class SettingsData
 			bool.isEditable = false;
 			#end
 			collection.addValue(bool, groupName);
+		}
+		
+		return collection;
+	}
+	
+	static public function exposeFileSettings(?collection:ExposedCollection, ?groupName:String):ExposedCollection
+	{
+		var bool:ExposedBool;
+		var floatDrag:ExposedFloatDrag;
+		var intDrag:ExposedIntDrag;
+		#if desktop
+		var path:ExposedPath;
+		#end
+		var select:ExposedSelect;
+		var str:ExposedString;
+		
+		if (collection == null) collection = new ExposedCollection();
+		
+		#if desktop
+		if (!collection.hasValue("filePath"))
+		{
+			path = new ExposedPath("filePath", "file path", "file path");
+			collection.addValue(path, groupName);
+		}
+		#end
+		
+		if (!collection.hasValue("fileName"))
+		{
+			str = new ExposedString("fileName", "file name", 0);// , "a-z A-Z 0-9 éèêàâôçùü.-_()");
+			collection.addValue(str, groupName);
+		}
+		
+		if (!collection.hasValue("compress"))
+		{
+			bool = new ExposedBool("compress");
+			collection.addValue(bool, groupName);
+		}
+		
+		if (!collection.hasValue("frameRateDefault"))
+		{
+			floatDrag = new ExposedFloatDrag("frameRateDefault", "default frame rate", 0, 300);
+			collection.addValue(floatDrag, groupName);
+		}
+		
+		if (!collection.hasValue("numFramesDefault"))
+		{
+			intDrag = new ExposedIntDrag("numFramesDefault", "default num frames", 1, 6000);
+			collection.addValue(intDrag, groupName);
+		}
+		
+		if (!collection.hasValue("numFramesAutoIncrease"))
+		{
+			bool = new ExposedBool("numFramesAutoIncrease", "auto increase num frames");
+			collection.addValue(bool, groupName);
+		}
+		
+		if (!collection.hasValue("tweenTransitionDefault"))
+		{
+			select = new ExposedSelect("tweenTransitionDefault", "default tween transition", Transitions.transitionNames, Transitions.transitionNames);
+			collection.addValue(select, groupName);
 		}
 		
 		return collection;
