bd99fe0ad230e35dfedfbe4c4c4764fbf9cb928b
Mathieu Sénidre <matse@skwatt.com>
2023-11-15 11:11:44 +0100

    Asset : register/unregister/update assets & values

--- src/valedit/asset/Asset.hx
+++ src/valedit/asset/Asset.hx
@@ -1,5 +1,8 @@
 package valedit.asset;
 import haxe.zip.Entry;
+import valedit.value.base.ExposedValue;
+import valeditor.ValEditorObject;
+import valeditor.ValEditorTemplate;
 
 /**
  * ...
@@ -11,8 +14,147 @@ abstract class Asset
 	public var path:String;
 	public var source:String;
 	public var isLoaded:Bool;
-
+	public var useCount(get, never):Int;
+	
+	private function get_useCount():Int { return this._registeredValues.length; }
+	
+	private var _registeredAssets:Array<Asset> = new Array<Asset>();
+	private var _registeredConstructorValues:Array<ExposedValue> = new Array<ExposedValue>();
+	private var _registeredValues:Array<ExposedValue> = new Array<ExposedValue>();
+	
 	public function new() 
+	{
+		
+	}
+	
+	public function registerAsset(asset:Asset):Void
+	{
+		// DEBUG
+		if (this._registeredAssets.indexOf(asset) != -1)
+		{
+			trace("asset already registered");
+		}
+		//\DEBUG
+		
+		this._registeredAssets[this._registeredAssets.length] = asset;
+	}
+	
+	public function registerConstructorValue(value:ExposedValue):Void
+	{
+		// DEBUG
+		if (this._registeredConstructorValues.indexOf(value) != -1)
+		{
+			trace("constructor value already registered");
+		}
+		//\DEBUG
+		
+		this._registeredConstructorValues[this._registeredConstructorValues.length] = value;
+	}
+	
+	public function registerValue(value:ExposedValue):Void
+	{
+		// DEBUG
+		if (this._registeredValues.indexOf(value) != -1)
+		{
+			trace("value already registered");
+		}
+		//\DEBUG
+		
+		this._registeredValues[this._registeredValues.length] = value;
+	}
+	
+	public function unregisterAsset(asset:Asset):Void
+	{
+		// DEBUG
+		if (this._registeredAssets.indexOf(asset) == -1)
+		{
+			trace("asset not registered or already unregistered");
+		}
+		//\DEBUG
+		
+		this._registeredAssets.remove(asset);
+	}
+	
+	public function unregisterConstructorValue(value:ExposedValue):Void
+	{
+		// DEBUG
+		if (this._registeredConstructorValues.indexOf(value) == -1)
+		{
+			trace("constructor value not registered or already unregistered");
+		}
+		//\DEBUG
+		
+		this._registeredConstructorValues.remove(value);
+	}
+	
+	public function unregisterValue(value:ExposedValue):Void
+	{
+		// DEBUG
+		if (this._registeredValues.indexOf(value) == -1)
+		{
+			trace("value not registered or already unregistered");
+		}
+		//\DEBUG
+		
+		this._registeredValues.remove(value);
+	}
+	
+	public function update():Void
+	{
+		var valObject:ValEditorObject;
+		var wasUpdateLocked:Bool;
+		var template:ValEditorTemplate;
+		var wasTemplateUpdateLocked:Bool = false;
+		for (value in this._registeredValues)
+		{
+			wasUpdateLocked = value.updateCollectionLocked;
+			if (!wasUpdateLocked) value.updateCollectionLocked = true;
+			valObject = value.valEditorObject;
+			if (valObject != null)
+			{
+				if (valObject.template != null)
+				{
+					template = cast valObject.template;
+					wasTemplateUpdateLocked = template.lockInstanceUpdates;
+					if (!wasTemplateUpdateLocked) template.lockInstanceUpdates = true;
+				}
+				else
+				{
+					template = null;
+				}
+				if (valObject.currentKeyFrame != null && valObject.currentKeyFrame.tween)
+				{
+					valObject.currentKeyFrame.resetTweens();
+				}
+			}
+			else
+			{
+				template = null;
+			}
+			value.value = this;
+			if (template != null)
+			{
+				template.lockInstanceUpdates = wasTemplateUpdateLocked;
+			}
+			if (valObject != null && valObject.currentKeyFrame != null && valObject.currentKeyFrame.tween)
+			{
+				valObject.currentKeyFrame.updateTweens();
+			}
+			value.updateCollectionLocked = wasUpdateLocked;
+		}
+		
+		for (value in this._registeredConstructorValues)
+		{
+			value.value = this;
+		}
+		
+		for (asset in this._registeredAssets)
+		{
+			asset.assetUpdate(this);
+		}
+	}
+	
+	public function assetUpdate(asset:Asset):Void
 	{
 		
 	}
