637c6c4c6448ed9684bd8c2f15d61576d90c7e23
Mathieu Sénidre <matse@skwatt.com>
2023-11-15 11:37:11 +0100

    StarlingTextureAsset register/unregister/update

--- src/valedit/asset/starling/StarlingTextureAsset.hx
+++ src/valedit/asset/starling/StarlingTextureAsset.hx
@@ -3,6 +3,8 @@ import haxe.zip.Entry;
 import openfl.display.BitmapData;
 import openfl.errors.Error;
 import starling.textures.Texture;
+import valedit.asset.Asset;
+import valedit.asset.BitmapAsset;
 import valeditor.utils.starling.TextureCreationParameters;
 
 /**
@@ -11,14 +13,40 @@ import valeditor.utils.starling.TextureCreationParameters;
  */
 class StarlingTextureAsset extends Asset 
 {
-	public var bitmapAsset:BitmapAsset;
+	public var bitmapAsset(get, set):BitmapAsset;
 	public var content:Texture;
 	public var preview:BitmapData;
 	public var textureParams:TextureCreationParameters;
 	
+	private var _bitmapAsset:BitmapAsset;
+	private function get_bitmapAsset():BitmapAsset { return this._bitmapAsset; }
+	private function set_bitmapAsset(value:BitmapAsset):BitmapAsset
+	{
+		if (this._bitmapAsset == value) return value;
+		
+		if (this._bitmapAsset != null)
+		{
+			this._bitmapAsset.unregisterAsset(this);
+		}
+		if (value != null)
+		{
+			value.registerAsset(this);
+		}
+		return this._bitmapAsset = value;
+	}
+	
 	public function new() 
 	{
 		super();
+	}
+	
+	override public function assetUpdate(asset:Asset):Void 
+	{
+		super.assetUpdate(asset);
+		
+		this.bitmapAsset = cast asset;
+		
+		ValEdit.assetLib.updateStarlingTexture(this, this._bitmapAsset, null, null, this.textureParams);
 	}
 	
 	override public function toJSONSave(json:Dynamic = null):Dynamic 
