a7f981ac306bda6ecb6d2cc81c43074d5ee47c20
Mathieu Sénidre <matse@skwatt.com>
2024-03-18 14:52:49 +0100

    ReadValuesExtra : reflect ValueExtra changes

--- src/valedit/value/extra/ReadValuesExtra.hx
+++ src/valedit/value/extra/ReadValuesExtra.hx
@@ -10,6 +10,14 @@ import valedit.value.extra.ValueExtra;
  */
 class ReadValuesExtra extends ValueExtra 
 {
+	static private var _POOL:Array<ReadValuesExtra> = new Array<ReadValuesExtra>();
+	
+	static public function fromPool(dispatchEventIfChange:Bool = false, ?valueNames:Array<String>):ReadValuesExtra
+	{
+		if (_POOL.length != 0) return _POOL.pop().setTo(dispatchEventIfChange, valueNames);
+		return new ReadValuesExtra(dispatchEventIfChange, valueNames);
+	}
+	
 	private var _dispatchEventIfChange:Bool;
 	private var _valueNames:Array<String> = new Array<String>();
 	
@@ -20,27 +28,46 @@ class ReadValuesExtra extends ValueExtra
 		if (valueNames != null) addValues(valueNames);
 	}
 	
+	override public function clear():Void 
+	{
+		this._valueNames.resize(0);
+		super.clear();
+	}
+	
+	public function pool():Void
+	{
+		clear();
+		_POOL[_POOL.length] = this;
+	}
+	
+	private function setTo(dispatchEventIfChange:Bool, ?valueNames:Array<String>):ReadValuesExtra
+	{
+		this._dispatchEventIfChange = dispatchEventIfChange;
+		if (valueNames != null) addValues(valueNames);
+		return this;
+	}
+	
 	public function addValue(propertyName:String):Void
 	{
-		_valueNames[_valueNames.length] = propertyName;
+		this._valueNames[this._valueNames.length] = propertyName;
 	}
 	
 	public function addValues(propertyNames:Array<String>):Void
 	{
 		for (propertyName in propertyNames)
 		{
-			_valueNames[_valueNames.length] = propertyName;
+			this._valueNames[this._valueNames.length] = propertyName;
 		}
 	}
 	
-	override public function clone():ValueExtra 
+	public function clone():ValueExtra 
 	{
-		var read:ReadValuesExtra = new ReadValuesExtra(this._dispatchEventIfChange, this._valueNames);
-		this.clone_internal(read);
+		var read:ReadValuesExtra = ReadValuesExtra.fromPool(this._dispatchEventIfChange, this._valueNames);
+		clone_internal(read);
 		return read;
 	}
 	
-	override public function applyToObject(object:Dynamic):Void 
+	public function applyToObject(object:Dynamic):Void 
 	{
 		var collection:ExposedCollection = null;
 		if (Std.isOfType(object, ValEditObject))
@@ -65,7 +92,7 @@ class ReadValuesExtra extends ValueExtra
 		}
 	}
 	
-	override public function execute():Void 
+	public function execute():Void 
 	{
 		var value:ExposedValue;
 		for (propertyName in this._valueNames)
