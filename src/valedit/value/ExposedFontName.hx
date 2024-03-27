7222a23f32f5e814000b3643472a1dc723882d8a
Mathieu Sénidre <matse@skwatt.com>
2024-03-27 18:39:05 +0100

    ExposedFontName -includeSystemFonts +fontSelection

--- src/valedit/value/ExposedFontName.hx
+++ src/valedit/value/ExposedFontName.hx
@@ -1,6 +1,7 @@
 package valedit.value;
 
 import valedit.value.base.ExposedValue;
+import valedit.value.data.FontSelection;
 
 /**
  * ...
@@ -15,18 +16,19 @@ class ExposedFontName extends ExposedValue
 		_POOL.resize(0);
 	}
 	
-	static public function fromPool(propertyName:String, name:String = null, includeSystemFonts:Bool = true):ExposedFontName
+	static public function fromPool(propertyName:String, name:String = null, fontSelection:String = FontSelection.ALL):ExposedFontName
 	{
-		if (_POOL.length != 0) return _POOL.pop().setTo(propertyName, name, includeSystemFonts);
-		return new ExposedFontName(propertyName, name, includeSystemFonts);
+		if (_POOL.length != 0) return _POOL.pop().setTo(propertyName, name, fontSelection);
+		return new ExposedFontName(propertyName, name, fontSelection);
 	}
 	
-	public var includeSystemFonts:Bool;
+	public var fontSelection:String;
 	
-	public function new(propertyName:String, name:String=null, includeSystemFonts:Bool = true) 
+	public function new(propertyName:String, name:String=null, fontSelection:String = FontSelection.ALL) 
 	{
 		super(propertyName, name);
-		this.includeSystemFonts = includeSystemFonts;
+		
+		this.fontSelection = fontSelection;
 	}
 	
 	public function pool():Void
@@ -35,16 +37,16 @@ class ExposedFontName extends ExposedValue
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setTo(propertyName:String, name:String, includeSystemFonts:Bool = true):ExposedFontName
+	private function setTo(propertyName:String, name:String, fontSelection:String = FontSelection.ALL):ExposedFontName
 	{
 		setNames(propertyName, name);
-		this.includeSystemFonts = includeSystemFonts;
+		this.fontSelection = fontSelection;
 		return this;
 	}
 	
 	override public function clone(copyValue:Bool = false):ExposedValue 
 	{
-		var font:ExposedFontName = fromPool(this.propertyName, this.name, this.includeSystemFonts);
+		var font:ExposedFontName = fromPool(this.propertyName, this.name, this.fontSelection);
 		super.clone_internal(font, copyValue);
 		return font;
 	}
@@ -52,12 +54,14 @@ class ExposedFontName extends ExposedValue
 	override public function fromJSON(json:Dynamic):Void 
 	{
 		super.fromJSON(json);
+		this.fontSelection = json.fontSelection;
 		this.value = json.value;
 	}
 	
 	override public function toJSON(json:Dynamic = null):Dynamic 
 	{
 		if (json == null) json = {};
+		json.fontSelection = this.fontSelection;
 		json.value = this.value;
 		return super.toJSON(json);
 	}
