dfa8c72743468e51c15e417abb6d1a4c22547d27
Mathieu Sénidre <matse@skwatt.com>
2024-03-27 18:38:26 +0100

    created FontSelection for ExposedFontName

---
+++ src/valedit/value/data/FontSelection.hx
@@ -0,0 +1,15 @@
+package valedit.value.data;
+
+/**
+ * ...
+ * @author Matse
+ */
+class FontSelection 
+{
+	
+	static public inline var ALL:String = "all";
+	static public inline var CUSTOM:String = "custom";
+	static public inline var EMBEDDED:String = "embedded";
+	static public inline var SYSTEM:String = "system";
+	
+}
