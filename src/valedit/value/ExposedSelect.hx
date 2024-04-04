42f67d9ef2f076d4010436b16742ba1d27ff0e2e
Mathieu Sénidre <matse@skwatt.com>
2024-04-04 16:54:22 +0200

    ExposedSelect now extends ExposedSelectBase

--- src/valedit/value/ExposedSelect.hx
+++ src/valedit/value/ExposedSelect.hx
@@ -1,12 +1,13 @@
 package valedit.value;
 
+import valedit.value.base.ExposedSelectBase;
 import valedit.value.base.ExposedValue;
 
 /**
  * ...
  * @author Matse
  */
-class ExposedSelect extends ExposedValue 
+class ExposedSelect extends ExposedSelectBase 
 {
 	static private var _POOL:Array<ExposedSelect> = new Array<ExposedSelect>();
 	
@@ -17,13 +18,9 @@ class ExposedSelect extends ExposedValue
 	
 	static public function fromPool(propertyName:String, name:String = null, choiceList:Array<String> = null, valueList:Array<Dynamic> = null, listPercentWidth:Float = 100):ExposedSelect
 	{
-		if (_POOL.length != 0) return _POOL.pop().setTo(propertyName, name, choiceList, valueList, listPercentWidth);
+		if (_POOL.length != 0) return cast _POOL.pop().setTo(propertyName, name, choiceList, valueList, listPercentWidth);
 		return new ExposedSelect(propertyName, name, choiceList, valueList, listPercentWidth);
 	}
-	
-	public var choiceList(default, null):Array<String>;
-	public var valueList(default, null):Array<Dynamic>;
-	public var listPercentWidth:Float;
 	
 	/**
 	   
@@ -35,19 +32,7 @@ class ExposedSelect extends ExposedValue
 	**/
 	public function new(propertyName:String, name:String = null, choiceList:Array<String> = null, valueList:Array<Dynamic> = null, listPercentWidth:Float = 100) 
 	{
-		super(propertyName, name);
-		if (choiceList == null) choiceList = new Array<String>();
-		if (valueList == null) valueList = new Array<Dynamic>();
-		this.choiceList = choiceList;
-		this.valueList = valueList;
-		this.listPercentWidth = listPercentWidth;
-	}
-	
-	override public function clear():Void 
-	{
-		super.clear();
-		this.choiceList = null;
-		this.valueList = null;
+		super(propertyName, name, choiceList, valueList, listPercentWidth);
 	}
 	
 	public function pool():Void
@@ -56,53 +41,10 @@ class ExposedSelect extends ExposedValue
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setTo(propertyName:String, name:String, choiceList:Array<String>, valueList:Array<Dynamic>, listPercentWidth:Float):ExposedSelect
-	{
-		setNames(propertyName, name);
-		if (choiceList == null) choiceList = new Array<String>();
-		if (valueList == null) valueList = new Array<Dynamic>();
-		this.choiceList = choiceList;
-		this.valueList = valueList;
-		this.listPercentWidth = listPercentWidth;
-		return this;
-	}
-	
-	/**
-	   if value is null choice is used as value
-	   @param	choice
-	   @param	value
-	**/
-	public function add(choice:String, value:Dynamic = null):Void
-	{
-		if (value == null) value = choice;
-		this.choiceList.push(choice);
-		this.valueList.push(value);
-		if (this.defaultValue == null) this.defaultValue = value;
-	}
-	
-	public function removeChoice(choice:String):Void
-	{
-		var index:Int = this.choiceList.indexOf(choice);
-		if (index != -1)
-		{
-			this.choiceList.splice(index, 1);
-			this.valueList.splice(index, 1);
-		}
-	}
-	
-	public function removeValue(value:Dynamic):Void
-	{
-		var index:Int = this.valueList.indexOf(value);
-		if (index != -1)
-		{
-			this.choiceList.splice(index, 1);
-			this.valueList.splice(index, 1);
-		}
-	}
-	
 	override public function clone(copyValue:Bool = false):ExposedValue 
 	{
-		var select:ExposedSelect = fromPool(this.propertyName, this.name, choiceList != null ? choiceList.copy():null, valueList != null ? valueList.copy():null, listPercentWidth);
+		var select:ExposedSelect = fromPool(this.propertyName, this.name, this.choiceList != null ? this.choiceList.copy():null, this.valueList != null ? this.valueList.copy():null, this.listPercentWidth);
+		select.selectOnKeyboardNavigation = this.selectOnKeyboardNavigation;
 		super.clone_internal(select, copyValue);
 		return select;
 	}
