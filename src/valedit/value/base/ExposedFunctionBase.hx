beead72999ec13b7039b46cbe760e9d273949964
Mathieu Sénidre <matse@skwatt.com>
2024-04-25 18:04:34 +0200

    ExposedFunctionBase +isParametersUIOpen
    also added missing code

--- src/valedit/value/base/ExposedFunctionBase.hx
+++ src/valedit/value/base/ExposedFunctionBase.hx
@@ -1,4 +1,5 @@
 package valedit.value.base;
+import valedit.value.base.ExposedValue;
 
 /**
  * ...
@@ -6,12 +7,29 @@ package valedit.value.base;
  */
 abstract class ExposedFunctionBase extends ExposedValue 
 {
+	#if valeditor
+	public var isParametersUIOpen:Bool = false;
+	#end
 	public var propagateTemplateFunctionCallToInstances:Bool = true;
 	
 	public function new(propertyName:String, name:String=null) 
 	{
 		super(propertyName, name);
-		
+	}
+	
+	override public function clear():Void 
+	{
+		#if valeditor
+		this.isParametersUIOpen = false;
+		#end
+		this.propagateTemplateFunctionCallToInstances = true;
+		super.clear();
+	}
+	
+	override function clone_internal(value:ExposedValue, copyValue:Bool = false):Void 
+	{
+		cast(value, ExposedFunctionBase).propagateTemplateFunctionCallToInstances = this.propagateTemplateFunctionCallToInstances;
+		super.clone_internal(value, copyValue);
 	}
 	
 	abstract public function getExposedValueParameters(?values:Array<ExposedValue>):Array<ExposedValue>;
