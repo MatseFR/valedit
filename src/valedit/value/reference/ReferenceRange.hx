93395236dea29987b5d9e5532fc05816d6449cef
Mathieu Sénidre <matse@skwatt.com>
2024-12-24 11:49:52 +0100

    created ReferenceRange

---
+++ src/valedit/value/reference/ReferenceRange.hx
@@ -0,0 +1,13 @@
+package valedit.value.reference;
+
+/**
+ * ...
+ * @author Matse
+ */
+class ReferenceRange 
+{
+	
+	inline static public var CONTAINER:String = "container";
+	inline static public var CONTAINER_LIBRARY:String = "container_library";
+	
+}
