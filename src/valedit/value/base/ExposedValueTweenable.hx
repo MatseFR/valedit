aebf9094533c40a1938c67b5695e15bc05582981
Mathieu Sénidre <matse@skwatt.com>
2025-09-15 18:56:10 +0200

    ExposedValueTweenable +isTweenableFunction

--- src/valedit/value/base/ExposedValueTweenable.hx
+++ src/valedit/value/base/ExposedValueTweenable.hx
@@ -11,10 +11,20 @@ abstract class ExposedValueTweenable extends ExposedValue
 		return this._isTweenable = value;
 	}
 	
+	/** if not null, this function will be called on collection updates to determine whether that value should be tweened or not */
+	public var isTweenableFunction:ExposedValueTweenable->Bool;
+	
 	public function new(propertyName:String, name:String=null) 
 	{
 		super(propertyName, name);
 		this._isTweenable = true;
+	}
+	
+	override public function clear():Void 
+	{
+		super.clear();
+		
+		this.isTweenableFunction = null;
 	}
 	
 }
