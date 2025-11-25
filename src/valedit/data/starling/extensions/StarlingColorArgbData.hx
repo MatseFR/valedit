c27ea4fd5f109e6b7bbed678bca268badef7b369
Mathieu Sénidre <matse@skwatt.com>
2025-11-25 21:25:51 +0100

    created StarlingColorArgbData

---
+++ src/valedit/data/starling/extensions/StarlingColorArgbData.hx
@@ -0,0 +1,32 @@
+package valedit.data.starling.extensions;
+import valedit.value.ExposedFloatDrag;
+
+/**
+ * ...
+ * @author Matse
+ */
+class StarlingColorArgbData 
+{
+
+	static public function exposeColorArgb(collection:ExposedCollection = null, groupName:String = null):ExposedCollection
+	{
+		var floatDrag:ExposedFloatDrag;
+		
+		if (collection == null) collection = new ExposedCollection();
+		
+		floatDrag = new ExposedFloatDrag("red", null, 0.0, 1.0, 0.025);
+		collection.addValue(floatDrag, groupName);
+		
+		floatDrag = new ExposedFloatDrag("green", null, 0.0, 1.0, 0.025);
+		collection.addValue(floatDrag, groupName);
+		
+		floatDrag = new ExposedFloatDrag("blue", null, 0.0, 1.0, 0.025);
+		collection.addValue(floatDrag, groupName);
+		
+		floatDrag = new ExposedFloatDrag("alpha", null, 0.0, 1.0, 0.025);
+		collection.addValue(floatDrag, groupName);
+		
+		return collection;
+	}
+	
+}
