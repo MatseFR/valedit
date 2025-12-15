85e33316b712b8f36c0b78b9bec6beb36bf9c924
Mathieu Sénidre <matse@skwatt.com>
2025-12-15 16:56:55 +0100

    StarlingColorArgbData : ui step set to 0.01

--- src/valedit/data/starling/extensions/StarlingColorArgbData.hx
+++ src/valedit/data/starling/extensions/StarlingColorArgbData.hx
@@ -14,16 +14,16 @@ class StarlingColorArgbData
 		
 		if (collection == null) collection = new ExposedCollection();
 		
-		floatDrag = new ExposedFloatDrag("red", null, 0.0, 1.0, 0.025);
+		floatDrag = new ExposedFloatDrag("red", null, 0.0, 1.0, 0.01);
 		collection.addValue(floatDrag, groupName);
 		
-		floatDrag = new ExposedFloatDrag("green", null, 0.0, 1.0, 0.025);
+		floatDrag = new ExposedFloatDrag("green", null, 0.0, 1.0, 0.01);
 		collection.addValue(floatDrag, groupName);
 		
-		floatDrag = new ExposedFloatDrag("blue", null, 0.0, 1.0, 0.025);
+		floatDrag = new ExposedFloatDrag("blue", null, 0.0, 1.0, 0.01);
 		collection.addValue(floatDrag, groupName);
 		
-		floatDrag = new ExposedFloatDrag("alpha", null, 0.0, 1.0, 0.025);
+		floatDrag = new ExposedFloatDrag("alpha", null, 0.0, 1.0, 0.01);
 		collection.addValue(floatDrag, groupName);
 		
 		return collection;
